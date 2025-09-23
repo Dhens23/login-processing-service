589fae98dc489bfe3f9daef8c880d55a8fe128f0
Daniel <a@a>
2025-09-23 11:16:19 +0200

    application and dependencies

---
+++ login-processing-service/src/main/java/com/assessment/login_processing_service/application/LoginProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.login_processing_service.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginProcessingServiceApplication.class, args);
+	}
+
+}
---
+++ login-processing-service/src/test/java/com/assessment/login_processing_service/application/LoginProcessingServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.assessment.login_processing_service.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginProcessingServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
