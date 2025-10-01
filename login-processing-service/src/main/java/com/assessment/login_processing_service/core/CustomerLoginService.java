badc3851a0922497c1b90ec5397bc1bebcb370b4
Daniel <a@a>
2025-10-01 15:23:04 +0200

    service transactional

--- login-processing-service/src/main/java/com/assessment/login_processing_service/core/CustomerLoginService.java
+++ login-processing-service/src/main/java/com/assessment/login_processing_service/core/CustomerLoginService.java
@@ -7,8 +7,10 @@ import com.assessment.login_processing_service.port.out.PublishLoginEventAdapter
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class CustomerLoginService implements CustomerLoginPort {
 
